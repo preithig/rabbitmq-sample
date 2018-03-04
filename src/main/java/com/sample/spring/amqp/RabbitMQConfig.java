f30904f2c6f9219d748ba5ca89324b1a478d712b
Ganapathy <PRGA@EUR.AD.SAG>
2018-03-04 12:58:57 +0530

    Fix. Msgs received by consumer

--- src/main/java/com/sample/spring/amqp/RabbitMQConfig.java
+++ src/main/java/com/sample/spring/amqp/RabbitMQConfig.java
@@ -1,7 +1,48 @@
 package com.sample.spring.amqp;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by PRGA on 3/3/2018.
  */
+@Configuration
+@EnableRabbit
 public class RabbitMQConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        return new CachingConnectionFactory("localhost");
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin(){
+        return new RabbitAdmin(connectionFactory());
+    }
+
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        template.setRoutingKey(getQueue().getName());
+        template.setQueue(getQueue().getName());
+        return  template;
+    }
+
+    @Bean
+    public Queue getQueue(){
+        return new Queue("myTestQueue");
+    }
 }
--- src/main/java/com/sample/spring/amqp/SpringReceiver.java
+++ src/main/java/com/sample/spring/amqp/SpringReceiver.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
  * Created by PRGA on 3/3/2018.
  */
-public class SpringSender {
+public class SpringReceiver {
 
-    public static void main(String[] args)
-    {
-        ApplicationContext context = new AnnotationConfigApplicationContext(RabbitMQConfig.class);
+
+    public static void main (String[] args){
+        ApplicationContext context  = new AnnotationConfigApplicationContext(RabbitMQConfig.class);
         AmqpTemplate template = context.getBean(AmqpTemplate.class);
-        template.convertAndSend("This is a test msg");
-        System.out.println("Sent message");
-    }
+
+        System.out.print("Message received " + template.receiveAndConvert());
+       }
 }
